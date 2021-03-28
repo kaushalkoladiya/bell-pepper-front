6620035f646d86694f6da31ac23148b2ecc8fe9d
Kaushal <kaushalkoladiya123@gmail.com>
2021-03-28 11:12:11 +0530

    Change API version

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "http://localhost:5000/";
-export const API_BASE_URL = `${BASE_URL}api`;
+export const API_BASE_URL = `${BASE_URL}api/v1`;
 export const ROOT_USER = "ROOT_USER";
 export const VENDOR_USER = "VENDOR_USER";
 export const CUSTOMER_USER = "CUSTOMER_USER";
